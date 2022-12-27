02b7c871e781d3ea03bb8add7a1032b6b9d47b01
FlutterCrypto <fluttercrypto@gmx.de>
2022-12-27 11:27:24 +0100

    updated

--- app/src/main/java/de/androidcrypto/navigationdrawerallactivities/BaseActivity.java
+++ app/src/main/java/de/androidcrypto/navigationdrawerallactivities/BaseActivity.java
@@ -14,6 +14,69 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
+/*
+
+android.support.design.widget.AppBarLayout
+com.google.android.material.appbar.AppBarLayout
+
+android.support.design.widget.BottomSheetDialog
+com.google.android.material.bottomsheet.BottomSheetDialog
+
+android.support.design.widget.TextInputLayout
+com.google.android.material.textfield.TextInputLayout
+
+android.support.design.widget.CoordinatorLayout
+androidx.coordinatorlayout.widget.CoordinatorLayout
+
+android.support.design.widget.NavigationView
+com.google.android.material.navigation.NavigationView
+
+androidx.core.view.ViewPager
+androidx.viewpager.widget.ViewPager
+
+android.support.design.widget.BottomSheetBehavior
+com.google.android.material.bottomsheet.BottomSheetBehavior
+
+android.support.v7.widget.Toolbar
+androidx.appcompat.widget.Toolbar
+
+android.support.design.internal.BottomNavigationItemView
+com.google.android.material.bottomnavigation.BottomNavigationItemView
+
+android.support.design.internal.BottomNavigationMenuView
+com.google.android.material.bottomnavigation.BottomNavigationMenuView
+
+androidx.appcompat.widget.CardView
+androidx.cardview.widget.CardView
+
+android.support.design.widget.BottomNavigationView
+com.google.android.material.bottomnavigation.BottomNavigationView
+
+androidx.core.view.ViewPager
+androidx.viewpager.widget.ViewPager
+
+importandroidx.core.widget.DrawerLayout
+androidx.drawerlayout.widget.DrawerLayout
+
+androidx.appcompat.widget.RecyclerView
+androidx.recyclerview.widget.RecyclerView
+
+androidx.core.view.PagerAdapter
+androidx.viewpager.widget.PagerAdapter
+
+importandroidx.core.app.FragmentManager
+importandroidx.fragment.app.FragmentManager
+
+android.support.v7.widget.AppCompatCheckBox
+androidx.appcompat.widget.AppCompatCheckBox
+
+android.support.design.R.id.snackbar_text
+com.google.android.material.R.id.snackbar_text
+
+android.support.design.widget.FloatingActionButton
+com.google.android.material.floatingactionbutton.FloatingActionButton
+ */
+
 public class BaseActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
